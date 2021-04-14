c5e3a807bd5a335e366e5d1ba212378b8959e735
Jacobdv <mrjacobdevries@gmail.com>
2021-04-14 16:50:52 -0700

    Working through old activities. Progress commit.

--- d3-data-journalism/assets/js/app.js
+++ d3-data-journalism/assets/js/app.js
@@ -1,3 +1,81 @@
+// Setting up SVG and display area.
+const svgWidth = 960;
+const svgHeight = 500;
+const margin = { top:20, right:40, bottom:80, left:100 };
+const width = svgWidth - (margin.left + margin.right);
+const height = svgHeight - (margin.top + margin.bottom);
+
+// Creating SVG in html.
+const svg = d3
+    .select('#scatter')
+    .append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+const chartGroup = svg
+    .append('g')
+    .attr('transfrom',`translate(${margin.left}, ${margin.top})`);
+
+
+
+// Creates axis.
+let chosenXAxis = 'age';
+
+function xScale(smokerData, chosenXAxis) {
+    const xLinearScale = d3
+        .scaleLinear()
+        .domain([d3.min(stateData, d => d[chosenXAxis]) * 0.8,
+                d3.max(stateData, d => d[chosenXAxis]) * 1.2])
+        .range([0, width]);
+    return xLinearScale;
+};
+
+function renderAxes(newXScale, xAxis) {
+    const bottomAxis = d3
+        .axisBottom(newXScale);
+    xAxis
+        .transition()
+        .duration(1000)
+        .call(bottomAxis);
+    return xAxis;
+};
+
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+    circlesGroup
+        .transition()
+        .duration(1000)
+        .attr('cx', d => newXScale(d[chosenXAxis]));
+    return circlesGroup;
+};
+
+function updateToolTip(chosenXAxis, circlesGroup){ 
+    let label;
+    if (chosenXAxis === 'age') {
+        label = 'Age';
+    } else {
+        label = 'Other';
+    }
+
+    const toolTip = d3
+        .tip()
+        .attr('class','tooltip')
+        .offset([80, -60])
+        .html(d => `${d.state}<br>${label} ${d[chosenXAxis]}`);
+
+    circlesGroup.call(toolTip);
+    circlesGroup
+        .on('mouseover', function(data) {
+            toolTip.show(data);
+        })
+        .on('mouseout', function(data) {
+            toolTip.hide(data);
+        });
+
+    return circlesGroup;
+};
+
+
+
+// This part actually uses the data csv file.
 d3.csv('assets/data/data.csv').then(stateData => {
 
     // Retyping data and creating object for used data.
@@ -31,6 +109,34 @@ d3.csv('assets/data/data.csv').then(stateData => {
             // 'smokesHigh': data.smokesHigh
         });
     });
-    console.log(smokerData);
+    
+    
+
+    /////////////////////
+
+    let xLinearScale = xScale(stateData, chosenXAxis);
+
+    const yLinearScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(stateData, d => d.smokes)])
+        .range([height, 0]);
+
+    const bottomAxis = d3.axisBottom(xLinearScale);
+    const leftAxis = d3.axisLeft(yLinearScale);
+
+    let xAxis = chartGroup
+        .append('g')
+        .call(leftAxis);
+
+    chartGroup
+        .append('g')
+        .call(leftAxis);
+
+    let circlesGroup = chartGroup
+        .selectAll('circle')
+        .data(stateData)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xLinearScale(d[chosenXAxis]))
 
 }).catch(error => console.log(error));
