6d5104bd1156780ab015038c3a3a3ae2cccf2d96
Jacobdv <mrjacobdevries@gmail.com>
2021-04-14 16:27:35 -0700

    Data retyping. Initial cleaning.

--- d3-data-journalism/assets/js/app.js
+++ d3-data-journalism/assets/js/app.js
@@ -1,1 +1,36 @@
-// @TODO: YOUR CODE HERE!
+d3.csv('assets/data/data.csv').then(stateData => {
+
+    // Retyping data and creating object for used data.
+    smokerData = [];
+    stateData.forEach(data => {
+    
+        // Retyping data.
+        data.id = +data.id;
+        data.poverty = +data.poverty;
+        data.povertyMoe = +data.povertyMoe;
+        data.age = +data.age;
+        data.ageMoe = +data.ageMoe;
+        data.income = +data.income;
+        data.incomeMoe = +data.incomeMoe;
+        data.healthcare = +data.healthcare;
+        data.healtcareLow = +data.healtcareLow;
+        data.healtcareHigh = +data.healtcareHigh;
+        data.obesity = +data.obesity;
+        data.obesityLow = +data.obesityLow;
+        data.obesityHigh = +data.obesityHigh;
+        data.smokes = +data.smokes;
+        data.smokesLow = +data.smokesLow;
+        data.smokesHigh = +data.smokesHigh;
+    
+        // Creating object for used data.
+        smokerData.push({
+            'state': data.abbr,
+            'age': data.age,
+            'smokes': data.smokes
+            // 'smokesLow': data.smokesLow,
+            // 'smokesHigh': data.smokesHigh
+        });
+    });
+    console.log(smokerData);
+
+}).catch(error => console.log(error));
