22fbcbf13b0917ebd0b8e76679bbd1d29e064fed
Jacobdv <mrjacobdevries@gmail.com>
2021-04-16 11:23:01 -0700

    Good progress. Y labels aren't showing up in the right spot.

--- d3-data-journalism/assets/js/app.js
+++ d3-data-journalism/assets/js/app.js
@@ -1,25 +1,84 @@
+/* 
+Set up for the chart size and SVG creation.
+*/
 // Initial parameters and definitions.
-const svgH = 960;
-const svgW = 500;
+const svgH = 500;
+const svgW = 960;
 const margin = { top:20, right:40, bottom:80, left:100 };
 const chartH = svgH - (margin.top + margin.bottom);
 const chartW = svgW - (margin.left + margin.right);
-let xVariable = 'smokes';
+let xVariable = 'age';
 let yVariable = 'income';
-
-
 // Add SVG element to scatter div.
 const svg = d3
     .select('#scatter')
     .append('svg')
     .attr('height', svgH)
     .attr('width', svgW);
-
 // Add chart group to the SVG.
 const chartGroup = svg
     .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+
+//////////////////////////////////////////////////
+
+
+/*
+Functions for drawing the chart and updating it.
+*/
+// Sets up x axis.
+function setupX(stateData, xVariable) {
+    const xScale = d3
+        .scaleLinear()
+        .range([0, chartW])
+        .domain([0, (d3.max(stateData, d => d[xVariable])) * 1.2]);
+    return xScale;
+};
+// Sets up y axis.
+function setupY(stateData, yVariable) {
+    const yScale = d3
+        .scaleLinear()
+        .range([chartH, 0])
+        .domain([0, (d3.max(stateData, d => d[yVariable])) * 1.2]);
+    return yScale;
+};
+// Draws circles.
+function createCircles(circlesGroup, xVariable) {
+    circlesGroup
+        .transition()
+        .duration(1000)
+        .attr('cx', d => newSetupX(d[xVariable]))
+        .attr('cy', d => newSetupY(d[yVariable]));
+    return circlesGroup;
+};
+// Draws x axis.
+function createX(newSetupX, xAxis) {
+    const bottomAxis = d3  
+        .axisBottom(newSetupX);
+    xAxis
+        .transition()
+        .duration(1000)
+        .call(bottomAxis);
+    return xAxis;
+};
+// Draws y axis.
+function createY(newSetupY, yAxis) {
+    const leftAxis = d3
+        .axisLeft(newSetupY);
+    yAxis
+        .transition()
+        .duration(1000)
+        .call(leftAxis);
+    return yAxis;
+};
+
+//////////////////////////////////////////////////
+
+
+/*
+Set up based on initial variables.
+*/
 // Import data.
 d3.csv('assets/data/data.csv').then(stateData => {
 
@@ -27,33 +86,28 @@ d3.csv('assets/data/data.csv').then(stateData => {
     stateData.forEach(data => {
         data.smokes = +data.smokes;
         data.income = +data.income;
+        data.obesity = +data.obesity;
+        data.age = +data.age;
     });
 
+    // Function calls.
     // Creating x scale and x axis on the chart.
-    const xScale = d3
-        .scaleLinear()
-        .range([0, chartW])
-        .domain([0, (d3.max(stateData, d => d.smokes)) * 1.2]);
-
+    let xScale = setupX(stateData, xVariable);
     const xAxis = d3
         .axisBottom(xScale);
-
     chartGroup
         .append('g')
+        .classed('x-axis', true)
         .attr('transform', `translate(0, ${chartH})`)
         .call(xAxis);
 
     // Creaeting y scale and y axis on the chart.
-    const yScale = d3
-    .scaleLinear()
-    .range([chartH, 0])
-    .domain([0, (d3.max(stateData, d => d.income)) * 1.2]);
-
+    let yScale = setupY(stateData, yVariable);
     const yAxis = d3
         .axisLeft(yScale);
-
     chartGroup
         .append('g')
+        .classed('y-axis', true)
         .call(yAxis);
 
     // Making the circles.
@@ -61,39 +115,106 @@ d3.csv('assets/data/data.csv').then(stateData => {
         .selectAll('circle')
         .data(stateData)
         .join('circle')
-        .attr('cx', d => xScale(d.smokes))
-        .attr('cy', d => yScale(d.income))
+        .attr('cx', d => xScale(d[xVariable]))
+        .attr('cy', d => yScale(d[yVariable]))
         .attr('r', '15')
         .attr('fill', 'cornflowerblue')
         .attr('opacity', 0.95)
         .attr('stroke', 'black')
         .attr('stroke-width', 1);
 
+    // Axes labels
+    // X Axis
+    const xGroup = chartGroup
+        .append('g')
+        .attr("transform", `translate(${chartW / 2}, ${chartH + 20})`);
+    const xAgeLabel = xGroup 
+        .append('text')
+        .attr('x', 0)
+        .attr('y', 20)
+        .attr('value', 'age')
+        .classed('active', false)
+        .text('Average Age');
+    const xIncomeLabel = xGroup 
+        .append('text')
+        .attr('x', 0)
+        .attr('y', 40)
+        .attr('value', 'income')
+        .classed('active', false)
+        .text('Median Income');
+    
+    // Y Axis
+    const yGroup = chartGroup
+        .append('g')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', 0 - (svgH / 2))
+        .attr('y', 0 - (margin.left - 30))
+        .attr('dy', '1em')
+        .attr('class', 'axisText');
+    const yAgeLabel = xGroup 
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', 0 - (svgH / 2))
+        .attr('y', 0 - (margin.left + 30))
+        .attr('value', 'age')
+        .classed('active', false)
+        .text('Average Age');
+    const yIncomeLabel = xGroup 
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', 0 - (svgH / 2))
+        .attr('y', 0 - (margin.left - 50))
+        .attr('value', 'income')
+        .classed('active', false)
+        .text('Median Income');
+
+//////////////////////////////////////////////////
+/*
+Set up for updating variables on click.
+*/
+
+
     // Labels for circles.
     stateData.forEach(d => {
         chartGroup
             .append('text')
-            .attr('x', (xScale(d.smokes)) - (12))
-            .attr('y', (yScale(d.income)) + (3))
+            .attr('x', (xScale(d[xVariable])) - (12))
+            .attr('y', (yScale(d[yVariable])) + (3))
             .attr('font-size', '15px')
             .text(d.abbr);
     }); 
 
-    // Labels for axes.
-    let yLabel = chartGroup
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('x', 0 - (margin.left + 40))
-        .attr('y', 0 - (chartH / 2))
-        .attr('dy', '1em')
-        .attr('class', 'axisText')
-        .text(`${yVariable}`);
+    // Event listener for label selection changes.
 
-    let xLabel = chartGroup
-        .append('text')
-        .attr('transform', `translate(${chartW / 2}, ${chartH + margin.top + 30})`)
-        .attr('class', 'axisText')
-        .text(`${xVariable} %`);
+    // Labels for axes.
+    // let yLabel = chartGroup
+    //     .append('text')
+    //     .attr('transform', 'rotate(-90)')
+    //     .attr('x', 0 - (svgH / 2))
+    //     .attr('y', 0 - (margin.left - 30))
+    //     .attr('dy', '1em')
+    //     .attr('class', 'axisText')
+    //     .text(`${yVariable}`);
+
+    // Listener for y variable selection changes.
+    // yLabel
+    //     .selectAll('text')
+    //     .on('click', function() {
+    //         const ySelection = d3
+    //             .select(this)
+    //             .attr('value');
+            
+    //         // Sets the y variable to the selected value.    
+    //         if (ySelection !== yVariable) {
+    //             yVariable = ySelection;
+    //         } 
+    //     })
+
+    // let xLabel = chartGroup
+    //     .append('text')
+    //     .attr('transform', `translate(${chartW / 2}, ${chartH + margin.top + 30})`)
+    //     .attr('class', 'axisText')
+    //     .text(`${xVariable}`);
 
 }).catch(error => console.log(error));
 
